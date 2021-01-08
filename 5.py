40b418c26e19e07e5855e4ae2523969871a81bda
Rohan Prinja <rohan.prinja@gmail.com>
2021-01-08 00:41:56 -0800

    5 comments

--- 5.py
+++ 5.py
@@ -1,3 +1,4 @@
+# euclidean algorithm
 def gcd(a, b):
     if a == 0:
         return b
@@ -11,6 +12,7 @@ def lcm(a, b):
     return a * b // gcd(a, b)
 
 # assumes n > 3
+# lcm(a, b, c) = lcm(lcm(a, b), c)
 def lcm_upto(n):
     ans = 2 # lcm(1, 2)
     for i in range(3, n):
